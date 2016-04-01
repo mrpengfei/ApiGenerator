654dae961378be5a409e07127e3f10420dba75cd
shipengfei <shipengfei@daishu360.com>
2016-04-01 14:24:58 +0800

    修改项目为winform

--- README.md
+++
@@ -1,2 +0,0 @@
-# ApiGenerator
-根据webapi生成客户端调用方法
---
+++ WebApiGenerator/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebApiGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateService service = new GenerateService();
+            service.Generate();
+            MessageBox.Show("生成完成");
+        }
+    }
+}
